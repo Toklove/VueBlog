46a8616f3ac1f080c03d5cfc2548712f5f8b56b8
下一页的记忆 <2586438083@qq.com>
2021-09-18 22:45:14 +0800

    Update Articles.vue

--- src/view/Articles.vue
+++ src/view/Articles.vue
@@ -6,8 +6,8 @@
         <header class="article-header">
           <div class="thumb">
             <div>
-              <n-skeleton v-if="skeleton" height="40px" width="60vh" :sharp="false"/>
-              <h1 v-else>{{ ArticlesConfig.title }}</h1>
+              <n-skeleton v-if="skeleton" height="40px" width="50vh" :sharp="false"/>
+              <h1>{{ ArticlesConfig.title }}</h1>
               <div class="post-meta">
                 <n-skeleton v-if="skeleton" height="20px" width="40%" :sharp="false"/>
                 <div v-else>
@@ -64,7 +64,6 @@ export default {
       tables: true, //默认为true。 允许支持表格语法。该选项要求 gfm 为true。
       breaks: true, //默认为false。 允许回车换行。该选项要求 gfm 为true。
       pedantic: false, //默认为false。 尽可能地兼容 markdown.pl的晦涩部分。不纠正原始模型任何的不良行为和错误。
-      sanitize: true, //对输出进行过滤（清理）
       smartLists: true,
       smartypants: true, //使用更为时髦的标点，比如在引用语法中加入破折号。
       langPrefix:"hljs language-"
